1cbdf8e2000f5219266dcc2cb5e2827541f467cd
Kekman <baxodirovich777@gmail.com>
2022-03-29 15:51:03 +0300

    fixed data floating

--- src/components/bar/AudioPlayer.tsx
+++ src/components/bar/AudioPlayer.tsx
@@ -166,7 +166,7 @@ export default function AudioPlayer({
       y: {
         display: false,
         max: settings.floating ? 150 : 300,
-        min: settings.floating ? -50 : 0,
+        min: settings.floating ? -20 : 0,
         ticks: {
           display: false,
         },
@@ -202,7 +202,7 @@ export default function AudioPlayer({
                 .reverse()
                 .concat(musicArray)
                 .map((data) => {
-                  return [-data * 0.1, data * 0.5];
+                  return [0, data * 0.5];
                 })
             : musicArray,
         fill: 1,
